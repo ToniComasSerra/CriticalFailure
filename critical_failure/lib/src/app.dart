ff6bc75b420aaaf403dadafff77366eac229b300
ToniComasSerra <antoniocomas@paucasesnovescifp.cat>
2022-01-12 16:24:05 +0100

    Inicio del proyecto
    Primeros aportes, añadido el documento de rutas para trabajar mas comodos y cada uno por su Page.

--- critical_failure/lib/src/app.dart
+++ critical_failure/lib/src/app.dart
@@ -1,8 +1,16 @@
+import 'package:critical_failure/src/pages/character_sheet_page.dart';
+import 'package:critical_failure/src/pages/home_page.dart';
+import 'package:critical_failure/src/routes/routes.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //home: HomePage(),
+      initialRoute: '/',
+      routes: getRoutes(),
+    );
   }
 }
---
+++ critical_failure/lib/src/pages/character_sheet_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CharacterSheet extends StatelessWidget {
+  const CharacterSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
