2d0e3366456f99a60685a01b4a16265cbf105819
ToniComasSerra <antoniocomas@paucasesnovescifp.cat>
2022-01-12 18:24:49 +0100

    Cambios para las paginas
    Acabado el paso de las paginas

---
+++ critical_failure/lib/src/utils/icona_string.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final _icons = <String, IconData>{
+  'character' : Icons.accessibility_new_rounded ,
+};
+
+Icon getIcon(String nomIcona){
+  return Icon(_icons[nomIcona],color: Colors.blue);
+}
