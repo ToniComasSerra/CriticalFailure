75862b1a78191c62ec6cd868bacb429500e54aa6
ToniComasSerra <antoniocomas@paucasesnovescifp.cat>
2022-01-17 16:33:54 +0100

    Restaurar chr_page
    Debido a constantes errores, se restaura la pagina del formulario

--- critical_failure/lib/src/pages/character_sheet_page.dart
+++ critical_failure/lib/src/pages/character_sheet_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:dropdown_formfield/dropdown_formfield.dart';
 
 class CharacterSheet extends StatefulWidget {
   const CharacterSheet({Key? key}) : super(key: key);
@@ -9,83 +8,14 @@ class CharacterSheet extends StatefulWidget {
 }
 
 class _CharacterSheetState extends State<CharacterSheet> {
-  String _raza = '';
-  String _resultado = '';
-  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
-  // La globalkey o formkey es una referencia al form mismo, tiene un uso standarizado en documentos oficiales
-
-  _saveForm() {
-    var form = formkey.currentState;
-    if (form!.validate()) {
-      form.save();
-      setState(() {
-        _resultado = _raza;
-      });
-    }
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Ficha de Personaje'),
       ),
-      body: Center(
-        child: Form(
-          key: formkey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(16),
-                // Esto es una variante del dropdrownbutton que se importa añadiendo la dependencia en el fichero pubspec.yalm
-                child: DropDownFormField(
-                  titleText: 'Elije tu raza',
-                  hintText: 'Elije una de las disponibles',
-                  value: _raza,
-                  onSaved: (value) {
-                    setState(() {
-                      _raza = value;
-                    });
-                  },
-                  onChanged: (value) {
-                    setState(() {
-                      _raza = value;
-                    });
-                  },
-                  dataSource: [
-                    {
-                      "display" : "Humano",
-                      "value" : "Humano",
-                    },
-                    {
-                      "display" : "Elfo",
-                      "value" : "Elfo",
-                    },
-                    {
-                      "display" : "Enano",
-                      "value" : "Enano",
-                    },
-                    {
-                      "display" : "Mediano",
-                      "value" : "Mediano",
-                    },
-                  ],
-                  textField: 'display',
-                  valueField: 'value',
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(8),
-                child: ElevatedButton(
-                  child: const Text('Guardar'),
-                  onPressed: _saveForm,
-                ),
-              )
-            ],
-          ),
-        ),
-      )
+      
     );
   }
 }
